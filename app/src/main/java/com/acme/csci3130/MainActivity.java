cacd50394a50b15bbfaeb145a1ee20f6932c0191
pachalB <broganpachal@gmail.com>
2017-05-10 18:27:52 -0300

    Adds new button and editText

--- app/src/main/java/com/acme/csci3130/MainActivity.java
+++ app/src/main/java/com/acme/csci3130/MainActivity.java
@@ -2,6 +2,10 @@ package com.acme.csci3130;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +13,12 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addText(View v){
+        EditText etEnterText = (EditText) findViewById(R.id.etEnterText);
+        Button btnAddText = (Button) findViewById(R.id.btnAddText);
+        TextView helloText = (TextView) findViewById(R.id.helloText);
+        helloText.setText(etEnterText.getText().toString());
     }
 }
